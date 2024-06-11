8c20e438b54f9a89edc2d72f8cf154378de556c8
GeorgyDzhanaev <georgy.dzhanaev@yandex.ru>
2024-06-11 15:19:21 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,16 @@
+a=12
+b=1.5
+c="Python, "
+d= b / a
+print (d)
+d=0.125
+name="всего задач:"
+secon_name="затрачено часов:"
+third_name="среднее время выполнение"
+print (f"Курс:{c}{name}{a}, {secon_name}{b}, {third_name} {d}часа.")
+#а теперь более правильный вартиант
+first_name = 12
+second_name = 1.5
+name = "Python"
+first_count = second_name / first_name
+print (f"Курс:{name}, всего задач:{first_name}, затрачено часов:{second_name}, среднее время выполнения {first_count}часа.")
